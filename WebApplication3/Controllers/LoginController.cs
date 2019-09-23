7ed1b76fe69c1abf553d582b52aa15bd8e0383af
LXY-1 <1143167344@qq.com>
2019-09-23 17:20:53 +0800

    添加项目文件。

---
+++ WebApplication3/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login ：url:xxx/Login进入这里调用控制器方法，也就是下面的Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
