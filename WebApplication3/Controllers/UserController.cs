e06206778302fe3d6cc36a2d472ce775e572112f
LXY-1 <1143167344@qq.com>
2019-09-24 12:49:53 +0800

    基于net mvc框架模板，结合自己的项目需求，进行二次改造，使其成为适合本系统的前端框架，主要是考虑项目整体骨架，模板页面，组件化页面，全局的配置比如全局ui框架，非全局的配置支持按需要在组件页面导入外部文件，不同功能模块的分离、解耦，方便多人开发，数据交互的全局配置，全局的菜单栏管理

---
+++ WebApplication3/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SearchUser()
+        {
+            return View();
+        }
+        // GET: User User/AddUser
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+    }
+}
